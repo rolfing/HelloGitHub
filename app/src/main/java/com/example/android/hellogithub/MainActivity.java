4930078875ef916f1a182b9543e7d0d2f76b297e
Michel Bourgeois <promotions332@gmail.com>
2016-09-20 13:48:36 -0700

    added function to button

--- app/src/main/java/com/example/android/hellogithub/MainActivity.java
+++ app/src/main/java/com/example/android/hellogithub/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnClicked(View v){
+        Context context = getApplicationContext();
+        CharSequence text = "Button Clicked!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
